9b471c552f6df8e23d633776bf2578d62251fb7d
Sierd Meijer <s.d.meijer@hotmail.com>
2017-10-27 16:54:09 +0200

    designing event forms

--- js/init.js
+++ js/init.js
@@ -3,7 +3,10 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-      $('.button-collapse').sideNav({
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+    $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 300
       edge: 'right', // Choose the horizontal origin
       closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
@@ -13,6 +16,14 @@
     }
   );
         
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
 
 
   }); // end of document ready
